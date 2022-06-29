c577edf2020850cde7c1bb92865695686bec5863
Vixmy <r.ys0118ys.b@gmail.com>
2022-06-29 12:21:04 +0900

    Revert "Revert "first commit""
    This reverts commit 0d02a5f8b9a7e8b47b3f82f01b6674ed39610195.

---
+++ HoughCircles/main.cpp
@@ -0,0 +1,12 @@
+# include <opencv2/opencv.hpp>
+
+#define PAT_ROW     7                   // チェスボードパターン列
+#define PAT_COL     10                  // チェスボードパターン行
+#define PAT_SIZE    PAT_ROW * PAT_COL   // チェスボードパターンサイズ
+#define CAM_W       720                 // カメラ画像幅
+#define CAM_H       540                 // カメラ画像高さ
+
+int main() {
+    
+    return 0;
+}
